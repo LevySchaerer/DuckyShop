a9e2b21ccdb422c36dcd27717c5ae5668dc23815
Levy Schaerer <levy.schaerer@ict-campus.net>
2025-06-04 22:14:52 +0200

    Shopping Cart without Localstorage

--- app/Components/Navigation/Navigation.jsx
+++ app/Components/Navigation/Navigation.jsx
@@ -1,18 +1,27 @@
+'use client';
+
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from './Navigation.module.css';
-
+import { GiShoppingCart } from "react-icons/gi";
+import Cart from '../Cart/Cart';
 
 const Navigation = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+
+    const toggleCart = () => {
+        setCartOpen(!cartOpen);
+    };
+
     return (
         <nav className={styles.nav}>
-
             <div className={styles.links}>
                 <ul>
-                    <li><Link href="/leaderboard" title="Leaderboard">Leaderboard</Link></li>
-                    <li><Link href="/login" title="Login">Login</Link></li>
-                    <li><Link href="/aboutus" title="About Us">About Us</Link></li>
+                    <li><Link href="/aboutus">About Us</Link></li>
+                    <li><Link href="/"><GiShoppingCart onClick={toggleCart} size={30} /></Link></li>
                 </ul>
             </div>
+            <Cart isOpen={cartOpen} onClose={toggleCart} />
         </nav>
     );
 };
