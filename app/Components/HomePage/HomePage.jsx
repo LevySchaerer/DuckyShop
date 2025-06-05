a4807b9a15b344c939696ddc3513b6bbb3aa9d7b
Levy Schaerer <levy.schaerer@ict-campus.net>
2025-06-05 15:54:36 +0200

    Product details

--- app/Components/HomePage/HomePage.jsx
+++ app/Components/HomePage/HomePage.jsx
@@ -5,29 +5,51 @@ import ducky from '../../public/RubberDucky.jpg'
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { useState } from "react";
 
-const images = [ducky, ducky, ducky]; // ggf. erweitern
+const products = [
+  {
+    id: 1,
+    name: "Ducky Mr Pop",
+    price: 2.99,
+    image: ducky
+  },
+  {
+    id: 2,
+    name: "Ducky Dr Max",
+    price: 2.99,
+    image: ducky
+  },
+  {
+    id: 3,
+    name: "Ducky Mrs Pop",
+    price: 2.99,
+    image: ducky
+  }
+];
 
 export default function HomePage() {
     const [index, setIndex] = useState(0);
 
-    const prev = () => setIndex((prev) => (prev - 1 + images.length) % images.length);
-    const next = () => setIndex((prev) => (prev + 1) % images.length);
+    const prev = () => setIndex((prev) => (prev - 1 + products.length) % products.length);
+    const next = () => setIndex((prev) => (prev + 1) % products.length);
 
     return (
         <div className={styles.body}>
             <div className={styles.container}>
+
                 <IoIosArrowBack size={60} onClick={prev} className={styles.arrow}/>
                 <div className={styles.sliderWrapper}>
                     <div
                         className={styles.slider}
                         style={{ transform: `translateX(-${index * 100}%)` }}
                     >
-                        {images.map((img, i) => (
-                            <div
-                                key={i}
-                                className={`${styles.slide} ${i === index ? styles.active : ''}`}
-                            >
-                                <Image src={img} alt="ducky" className={styles.ducky} />
+                        {products.map((product, i) => (
+                            <div key={i} className={`${styles.slide} ${i === index ? styles.active : ''}`}>
+                                <Image src={product.image} alt="ducky" className={styles.ducky} />
+                                <div className={styles.details}>
+                                    <h1>{product.price} Fr</h1>
+                                    <h2>{product.name}</h2>
+                                    <button onClick={() => console.log(product.id)}>Add to Cart</button>
+                                </div>
                             </div>
                         ))}
                     </div>
