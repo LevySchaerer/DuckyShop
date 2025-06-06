689e9493082489404472346b909dc9c03ab38168
Levy Schaerer <levy.schaerer@ict-campus.net>
2025-06-06 16:02:45 +0200

    Checkout Design

--- app/Components/Checkout/Checkout.jsx
+++ app/Components/Checkout/Checkout.jsx
@@ -7,53 +7,56 @@ const Checkout = () => {
     const [sum, setSum] = useState(0)
     const [cart, setCart] = useState([])
 
-        useEffect(() => {
-            const cartdata = localStorage.getItem('cart');
-            const cartjson = cartdata ? JSON.parse(cartdata) : [];
-            setCart(cartjson)
-            console.log(cartjson)
-            const stockSum = cartjson.reduce((sum, product) => sum + (product.stock * product.price), 0)
-            setSum(stockSum);
-        }, [])
+    useEffect(() => {
+        const cartdata = localStorage.getItem('cart');
+        const cartjson = cartdata ? JSON.parse(cartdata) : [];
+        setCart(cartjson)
+        console.log(cartjson)
+        const stockSum = cartjson.reduce((sum, product) => sum + (product.stock * product.price), 0)
+        setSum(stockSum);
+    }, [])
 
     return (
         <div className={styles.body}>
             <div className={styles.checkoutContainer}>
                 <div className={styles.leftSection}>
-                    <h1>Checkout</h1>
-                    <h2>Delivery</h2>
+                    <h1 className={styles.title}>Checkout</h1>
+                    <h2 className={styles.sectionTitle}>Delivery</h2>
 
-                    <h3>First Name</h3>
-                    <input placeholder="Saul" type="text" />
-                    <h3>Name</h3>
-                    <input placeholder="Goodman" type="text" />
-                    <h3>Mobile</h3>
-                    <input placeholder="+41 77 483 97 83" type="text" />
-                    <h3>Adress</h3>
-                    <input placeholder="Broadway 63" type="text" />
-                    <h3>PLZ</h3>
-                    <input placeholder="Tarrytown 10591" type="text" />
-                    <h3>Ciry</h3>
-                    <input placeholder="New York" type="text" />
+                    <h3 className={styles.CheckoutInputTitel}>First Name</h3>
+                    <input className={styles.CheckoutInput} placeholder="Saul" type="text" />
+                    <h3 className={styles.CheckoutInputTitel}>Name</h3>
+                    <input className={styles.CheckoutInput} placeholder="Goodman" type="text" />
+                    <h3 className={styles.CheckoutInputTitel}>Mobile</h3>
+                    <input className={styles.CheckoutInput} placeholder="+41 77 483 97 83" type="text" />
+                    <h3 className={styles.CheckoutInputTitel}>Adress</h3>
+                    <input className={styles.CheckoutInput} placeholder="Broadway 63" type="text" />
+                    <h3 className={styles.CheckoutInputTitel}>PLZ</h3>
+                    <input className={styles.CheckoutInput} placeholder="Tarrytown 10591" type="text" />
+                    <h3 className={styles.CheckoutInputTitel}>Ciry</h3>
+                    <input className={styles.CheckoutInput} placeholder="New York" type="text" />
                 </div>
                 <div className={styles.rightSection}>
-                    <h2>Payment</h2>
+                    <h2 className={styles.sectionTitle}>Payment</h2>
 
-                    {cart.map((product, i) => {
-                        return (
-                            <div key={i} className={styles.cartItem}>
-                                <div className={styles.leftCartSection}>
-                                    <Image src={product.image} alt="ducky" className={styles.ducky} />
-                                    <div className={styles.leftDetails}>
-                                        <h5 className={styles.name}>{product.name}</h5>
-                                        <h5>stock {product.stock}</h5>
+                    <div className={styles.CartItems}>
+                        {cart.map((product, i) => {
+                            return (
+                                <div key={i} className={styles.cartItem}>
+                                    <div className={styles.leftCartSection}>
+                                        <Image src={product.image} alt="ducky" className={styles.ducky} />
+                                        <div className={styles.leftDetails}>
+                                            <h5 className={styles.name}>{product.name}</h5>
+                                            <h5>stock {product.stock}</h5>
+                                        </div>
                                     </div>
+                                    <h4 className={styles.price}>{product.price} Fr</h4>
                                 </div>
-                                <h4 className={styles.price}>{product.price} Fr</h4>
-                            </div>
-                        )
-                    })}
-                    <h2 className={styles.totalPrice}>Total: <span>{sum.toFixed(1)}0 Fr</span></h2>
+                            )
+                        })}
+                    </div>
+                
+                    <h3 className={styles.totalPrice}>Total: <span>{sum.toFixed(1)}0 Fr</span></h3>
                     
                     <div className={styles.paymentSection}>
                         <p className={styles.paymentInfo}><FaInfo className={styles.infoIcon}/>Please do not change the amount in the App, otherwise the order cannot be delivered.</p>
