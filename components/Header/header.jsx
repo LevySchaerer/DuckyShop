4e23cc3befb0b34e2a2a868e048c43a72fb8d8fa
Cyrill Gammeter <cyrill.gammeter@ict-campus.net>
2025-06-12 13:01:25 +0200

    Change Imports in footer and header

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
-import styles from './Header.module.css';
+import styles from './header.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
