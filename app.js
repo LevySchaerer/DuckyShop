302d4e46631bd8a1c0806f9dd343aabb35d6fd60
Cyrill Gammeter <cyrill.gammeter@ict-campus.net>
2025-06-17 22:41:47 +0200

    Modified app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const AppPage = () => {
   return (
     <div>
       <h1>App Page</h1>
-      <p>This is a valid Next.js page component.</p>
+      <p>This is a valid Next.js page component. </p>
     </div>
   );
 };
